b02874c3267adb06929eb5a3a1281dda8177561c
Ivan Petrakov <rebyatyonok@yandex.com>
2022-06-28 15:55:21 +0300

    project init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <GameVue
+    :is-paused="isPaused"
+  />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import GameVue from './components/Game.vue';
+
+const isPaused = ref(false)
+</script>
+
+<style>
+@import "./style/global.css";
+
+#app {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+    font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/Game.vue
@@ -0,0 +1,10 @@
+<template>
+  <div v-if="isPaused">Press SPACE to continue</div>
+  <div v-else>Here the game will be</div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  isPaused: boolean
+}>()
+</script>
