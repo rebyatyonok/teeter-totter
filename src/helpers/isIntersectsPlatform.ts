5e9f4e0f18dbfaf0f1175b259d576311baaed821
Ivan Petrakov <rebyatyonok@yandex.com>
2022-07-04 00:48:41 +0300

    rename

--- src/helpers/isIntersectsPlatform.ts
+++ src/helpers/isIntersectsPlatform.ts
@@ -1,16 +1,16 @@
-export function isFigureSettled(figure: HTMLElement | null, beam: HTMLElement | null, angle: number = 0) {
-	if (!figure || !beam) {
+export function isIntersectsPlatform(figure: HTMLElement | null, platform: HTMLElement | null, angle: number = 0) {
+	if (!figure || !platform) {
 		// just for type safety
 		throw Error("Argument is undefined!")
 	}
 
 	const figureRect = figure.getBoundingClientRect();
-	const beamRect = beam.getBoundingClientRect();
+	const beamRect = platform.getBoundingClientRect();
 
 	// if beam has angle we need add/substract difference for more precise calculation
 	// we will do it by calculation one of the sides of right triangle
 	if (angle !== 0) {
-		const isLeft = parseInt(figure.style.left) < beam.offsetWidth / 2
+		const isLeft = parseInt(figure.style.left) < platform.offsetWidth / 2
 
 		const shouldAddFigureWidth = isLeft ? angle < 0 : angle > 0
 		const left = parseInt(shouldAddFigureWidth ? figure.style.left + figure.offsetWidth : figure.style.left)
